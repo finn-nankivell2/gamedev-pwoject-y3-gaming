faa13f6c9c81167f068d595e28de1c477d0751f5
finn-nankivell2 <N00221795@iadt.ie>
2024-11-22 22:40:07 +0000

    working maybe

--- Assets/Scripts/PlayerMovement2.cs
+++ Assets/Scripts/PlayerMovement2.cs
@@ -25,6 +25,42 @@ public class PlayerMovement2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ySpeed -= gravityForce;
+        if(characterController.isGrounded)
+        {
+            ySpeed = 0;
+        }
 
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            ySpeed = jumpForce;
+        }
+
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        Vector3 inputDirection = new (horizontalInput, 0, verticalInput);
+
+        Vector3 moveDirection = mainCamera.TransformDirection(inputDirection);
+        Vector3 flatMoveDirection = new (moveDirection.x, 0, moveDirection.z);
+
+        velocity = flatMoveDirection * maxSpeed;
+        velocity.y = ySpeed;
+
+        LimitSpeed();
+        characterController.Move(velocity * Time.deltaTime);
+
+		var norm = new Vector3(velocity.normalized.x, 0f, velocity.normalized.z);
+		if (norm.magnitude > 0f) {
+			transform.forward = norm;
+		}
+    }
+
+    void LimitSpeed()
+    {
+        Vector3 flatVelocity = new Vector3(velocity.x, 0, velocity.z);
+        if(flatVelocity.magnitude > maxSpeed){
+            Vector3 limitedFlatVelocity = flatVelocity.normalized * maxSpeed;
+            velocity = new (limitedFlatVelocity.x, velocity.y, limitedFlatVelocity.z);
+        }
     }
 }
