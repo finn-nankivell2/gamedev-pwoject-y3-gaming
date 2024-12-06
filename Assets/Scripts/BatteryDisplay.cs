e3dec8747f137eb5c0386276024194be795b270f
sylvie <N00220460@iadt.ie>
2024-12-06 15:29:32 +0000

    air jumps visualized on battery display

--- Assets/Scripts/BatteryDisplay.cs
+++ Assets/Scripts/BatteryDisplay.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
 
 public class BatteryDisplay : MonoBehaviour
@@ -9,29 +10,53 @@ public class BatteryDisplay : MonoBehaviour
     private List<GameObject> batterySprites = new List<GameObject>();
     public GameObject batteryPrefab;
     public float batteryOffset = 100;
-    private int uiAirJumps = 0;
+    private int uiMaxAirJumps = 0;
+    private int uiCurrentAirJumps = 0;
     private PlayerMovementFreecam playerScript;
 
     // Start is called before the first frame update
     void Start()
     {
         playerScript = GameManager.Instance.playerScript;
-        uiAirJumps = playerScript.maxAirJumps;
-        for (int i=0; i < uiAirJumps; i++) {
-            AddBatterySprite();
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (playerScript.maxAirJumps > uiAirJumps) {
-            int diff = playerScript.maxAirJumps - uiAirJumps;
+        if (playerScript.maxAirJumps > uiMaxAirJumps) {
+            int diff = playerScript.maxAirJumps - uiMaxAirJumps;
             for (int i=0; i < diff; i++) {
                 AddBatterySprite();
             }
+            uiMaxAirJumps = playerScript.maxAirJumps;
         }
-        uiAirJumps = playerScript.maxAirJumps;
+
+        if (playerScript.airJumps < uiCurrentAirJumps) {
+            int diff = playerScript.maxAirJumps - playerScript.airJumps;
+            int idx = batterySprites.Count - diff;
+
+            Image img = batterySprites[idx].GetComponent<Image>();
+            img.color = Color.black;
+            uiCurrentAirJumps = playerScript.airJumps;
+        }
+
+        if (playerScript.airJumps > uiCurrentAirJumps) {
+            foreach (GameObject battery in batterySprites) {
+                Image img2 = battery.GetComponent<Image>();
+                img2.color = Color.white;
+
+            }
+            uiCurrentAirJumps = playerScript.airJumps;
+        }
+
+        for(int i=0; i<uiMaxAirJumps; i++)
+        {
+            if(i < playerScript.airJumps)
+            {
+
+            }
+        }
+
     }
 
     void AddBatterySprite()
